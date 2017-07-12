3dcda578714c9d8463f99ba80edf604b534ca4c6
Eugen <jk@47.kiev.ua>
2017-07-12 12:23:17 +0300

    Using shopFrontendCartAddController instead of writing own code.

--- lib/actions/shopMultiaddPluginCartAdd.controller.php
+++ lib/actions/shopMultiaddPluginCartAdd.controller.php
@@ -6,37 +6,15 @@ class shopMultiaddPluginCartAddController extends waJsonController
 
     public function execute()
     {
-
-        $code = waRequest::cookie('shop_cart');
-        if (!$code) {
-            $code = md5(uniqid(time(), true));
-            // header for IE
-            wa()->getResponse()->addHeader('P3P', 'CP="NOI ADM DEV COM NAV OUR STP"');
-            // set cart cookie
-            wa()->getResponse()->setCookie('shop_cart', $code, time() + 30 * 86400, null, '', false, true);
-        }
-        $cart = new shopCart($code);
-        $product_sku_model = new shopProductSkusModel();
+        $cartAdd = new shopFrontendCartAddController();
 
         $items = waRequest::post('items', array(), waRequest::TYPE_ARRAY);
 
         foreach ($items as $i) {
-
-            $sku = $product_sku_model
-                ->where('id=? AND product_id=?', ifempty($i['sku_id']), ifempty($i['product_id']))
-                ->fetch();
-
-            if(!$sku) {
-                continue;
-            }
-            $item = array(
-                'type' => 'product',
-                'product_id' => $i['product_id'],
-                'sku_id' => $i['sku_id'],
-                'quantity' => ifempty($i['quantity'], 1),
-            );
-            $cart->addItem($item);
+            $_POST = $i;
+            $cartAdd->execute();
         }
+        $cart = new shopCart();
         $this->response = array(
             'total' => $cart->total(),
             'count' => $cart->count(),
